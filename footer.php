f35f785caf76d21135325d68baccee724b12a525
Emory Griffith <emorygriffith91@gmail.com>
2017-01-29 22:53:43 -0500

    styles on footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
 			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 
-				<div id="inner-footer" class="wrap cf">
+				<div id="inner-footer" class="wrap cf container">
 					<nav role="navigation">
 						<?php wp_nav_menu(array(
     					'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
