1b81b68c0159c3ff124f07fa5b0cc682873827a2
Emory Griffith <emorygriffith91@gmail.com>
2017-01-29 22:14:28 -0500

    2 galleries successfully bootsrapped. need to add media queries for the screenshots and also make footer sticky

--- page-home.php
+++ page-home.php
@@ -9,44 +9,65 @@
 			<div class="banner"></div>
 
 			<div class="container galleries">
-				<ul class="col-lg-8 gallery">
-					<h2>Vermont</h2>
+
+						<!-- Gallery #1 -->
+						<div class="col-lg-9 gallery row">
+						<h2>Vermont</h2>
 						<?php
-								$query = new WP_Query( array( 'post_type' => 'trails' ) );
-								if ( $query->have_posts() ) : ?>
-									<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-											<div class="gallery-card">
-													<h2><?php the_title(); ?></h2>
-													<div class="gallery-content">
-
-														<!-- ACF Query for Placeholder Images -->
-														<?php
-														$image = get_field('placeholder_image');
-														if( !empty($image) ): ?>
-															<img class="" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-														<?php endif; ?>
-
-
-													</div>
-
-
-
-
-											</div>
-
-
-
-
-
-
-									<?php endwhile; wp_reset_postdata(); ?>
+						$query = new WP_Query( array( 'post_type' => 'trails' ) );
+						if ( $query->have_posts() ) : ?>
+						<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+								<div class="col-md-4">
+									<div class="gallery-card">
+										<div class="gallery-content">
+											<!-- ACF Query for Placeholder Images -->
+											<?php
+											$image = get_field('placeholder_image');
+											if( !empty($image) ): ?>
+											<img class="gallery-img" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+											<?php endif; ?>
+										</div><!--gallery content -->
+									</div><!--gallery card -->
+								</div> <!--col-md-4 -->
+						<?php endwhile; wp_reset_postdata(); ?>
 						<!-- show pagination here -->
 						<?php else : ?>
-							<!-- show 404 error here -->
+						<!-- show 404 error here -->
 						<?php endif; ?>
+						</div><!--gallery -->
 
 
-			</div>
+						<!-- Gallery #2 -->
+						<div class="col-lg-9 gallery row">
+						<h2>New Hampshire</h2>
+						<?php
+						$query = new WP_Query( array( 'post_type' => 'trails' ) );
+						if ( $query->have_posts() ) : ?>
+						<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+								<div class="col-md-4">
+									<div class="gallery-card">
+										<div class="gallery-content">
+											<!-- ACF Query for Placeholder Images -->
+											<?php
+											$image = get_field('placeholder_image');
+											if( !empty($image) ): ?>
+											<img class="gallery-img" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+											<?php endif; ?>
+										</div><!--gallery content -->
+									</div><!--gallery card -->
+								</div> <!--col-md-4 -->
+						<?php endwhile; wp_reset_postdata(); ?>
+						<!-- show pagination here -->
+						<?php else : ?>
+						<!-- show 404 error here -->
+						<?php endif; ?>
+						</div><!--gallery -->
+
+
+
+			</div><!--galleries -->
+
+
 
 
 
