629db38b90e2f69e2f37b80f89b760d401317c82
Emory Griffith <emorygriffith91@gmail.com>
2017-05-14 20:50:52 -0400

    refactored approach and about page to share styles for boxes and banner and page title.

--- page-about.php
+++ page-about.php
@@ -6,13 +6,54 @@
 
 <?php get_header(); ?>
 
-			<div class="col-lg-9">
-				<h2>About Monarch Capital</h2>
-				<p>Lorum ipsum Ut sea noster aliquam, mollis commune similique ea vim. Est albucius appellantur ad. Et vix ceteros maiestatis, partem senserit elaboraret pro ne. Tale scripta mea ea, quo at prima debet omittantur, ex cetero officiis eleifend ius. Cu errem oratio partiendo mea, agam eirmod torquatos ut ius, audire disputationi te has.
-Enim augue doctus nec ad. Eos te tempor albucius. Debet scaevola vim ut, commodo eleifend philosophia at his, everti tibique sed id. Iusto possim in mei, dolorem accusam nam ex.
-Verterem salutatus te pri, stet libris probatus ut eum. Ad veri volumus eam. Has recusabo iracundia no. Et alterum inermis vel, erat evertitur reprehendunt usu et, dicit tamquam similique te his. Omnesque adolescens eu eos, te vis elitr senserit dignissim.</p>
+<div class="row col-lg-9 page-header">
+	<h1>Goal & Mission</h1>
+	<h2>Create exceptional value through financial services for clients and employees</h2>
+</div>
+
+<div class="row col-lg-9 page-middle">
+			<div class="col-lg-6 col-md-6 col-sm-6 shadow">
+				<h3>Preservation</h3>
+				<p>We are dedicated to the preservation and and growth of our clients' wealth, maintaining our unique culture and honoring the legacy of our firm's name </p>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6">
+				<h3>Entrepreneurship</h3>
+				<p>We believe in fostering individual creativity and a sense of ownership</p>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6">
+				<h3>Integrity</h3>
+				<p>In every action we take, we do the right thing for the right reasons</p>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 shadow">
+				<h3>Talent</h3>
+				<p>We conclude that great organizations are built by employing and partnering with the best and brightest people</p>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 shadow">
+				<h3>Excellence</h3>
+				<p>We pride ourselves on the quality of our work and seek to exceed expectations</p>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6">
+				<h3>Innovation</h3>
+				<p>We strive to create opportunities with new approaches and resources</p>
 			</div>
 
+</div>
 
+
+
+<div class="row approach-lower">
+	<a href="/services">
+		<div class="col-lg-6 col-md-6 col-sm-6 approach-services">
+			<h3>Learn more about our <span style="text-decoration:underline;">Services</span> ></h3>
+		</div>
+	<a/>
+
+	<a href="/team">
+		<div class="col-lg-6 col-md-6 col-sm-6 approach-team">
+			<h3>Learn more about our <span style="text-decoration:underline;">Team</span> > </h3>
+		</div>
+	<a/>
+
+</div>
 
 <?php get_footer(); ?>
--- page-approach.php
+++ page-approach.php
@@ -6,23 +6,61 @@
 
 <?php get_header(); ?>
 
-			<div class="row col-lg-9 approach-header">
+			<div class="row col-lg-9 page-header">
 				<h1>Approach</h1>
 				<h2>Our winning methodology consistently <br>yields exceptional results</h2>
 			</div>
+
+			<div class="row col-lg-9 page-middle">
+						<div class="col-lg-6 col-md-6 col-sm-6 shadow">
+							<h3>Nationally Trusted</h3>
+							<p>Annual review by a recognized <br> National Accounting Firm</p>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6">
+							<h3>Multi-Discplinary Team</h3>
+							<p>Integrated team of professionals <br> focused on creating long-term solutions</p>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6">
+							<h3>Dynamic Process</h3>
+							<p>Our work grows and changes as <br> the circumstances in your life evolve</p>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 shadow">
+							<h3>Close Cooperation</h3>
+							<p>Work in close cooperation with <br> our clients and their advisors to meet needs</p>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 shadow">
+							<h3>Full Background Checks</h3>
+							<p>Performed on all advisors/employees</p>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6">
+							<h3>Quarterly Reviews</h3>
+							<p>Brand management and business objectives</p>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6">
+							<h3>Hollistic View</h3>
+							<p>A non-conflicted unique <br> approach to risk assessment</p>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 shadow">
+							<h3>Individualized Service</h3>
+							<p>High level systems ensure excellent <br> service to our clients</p>
+						</div>
+			</div>
+
 
 
 			<div class="row approach-lower">
 				<a href="/services">
 					<div class="col-lg-6 col-md-6 col-sm-6 approach-services">
-						<h3>Learn more about our Services ></h3>
+						<h3>Learn more about our <span style="text-decoration:underline;">Services</span> ></h3>
 					</div>
 				<a/>
 
 				<a href="/team">
 					<div class="col-lg-6 col-md-6 col-sm-6 approach-team">
-						<h3>Learn more about our Team > </h3>
+						<h3>Learn more about our <span style="text-decoration:underline;">Team</span> > </h3>
 					</div>
 				<a/>
+
+			</div>
 
 <?php get_footer(); ?>
