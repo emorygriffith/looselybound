66517eaa02112845d1fa7c542957980bb8c0591f
Emory Griffith <emorygriffith91@gmail.com>
2017-06-25 22:12:42 -0400

    cleaned up bootstrap cols an rows for TEAM section of about

--- page-about.php
+++ page-about.php
@@ -41,118 +41,123 @@
 
 
 <div class="row col-lg-12 about-bios">
-	<div class="">
+
 		<h2 id="team">TEAM</h2>
-		<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-			<!-- <img class="" src="<?php echo get_template_directory_uri(); ?>/library/images/nothumb.gif" alt="..."> -->
-			<h4 class="name">Barry Klarbrg</h4>
-			<h4>Founder</h4>
-			<p>
-					Barry Klarberg, a CPA, formed Monarch Wealth and Business Management LLC in 2008,
-					a full service Family Office firm specializing in providing Business and Wealth
-					Management Services to High Net-Worth
-					Individuals, Athletes and Entertainers.
-					Preceding Monarch, Barry spent two years as Senior
-					Managing Director at Guggenheim Partners, LLC
-					heading up Guggenheim Wealth Services New York
-					and Los Angeles offices. His team managed over 100
-					client relationships. Prior to joining Guggenheim
-					Partners, Barry was responsible for managing Assante
-					Wealth Services and Loring Ward Private Family Office
-					from January 1, 2000 to August 15, 2006.
-					Barry began his career at Deloitte in 1981, where he
-					spent 11 years. He was the Firm's National Director of
-					Sports and Entertainment. In 1992, he formed KRT
-					Business Management, specializing in Wealth
-					Management Services which he eventually sold to
-					Assante.
-				</br></br>
-					Barry has been internationally recognized as a leader in
-					the area of financial counseling to Athletes,
-					Entertainers, High Net Worth individuals and Family
-					Offices. Barry was elected to the "Sports Business
-					Journals' 40 under 40." He regularly appears on CNN,
-					CNBC and The Wall Street Journal Report. Barry is
-					actively involved in various charity and community
-					activities including the National Meningitis
-					Foundation.
-					Barry received a BBA (1981) and MS in Taxation (1985)
-					from Pace University and maintains his Series 65. He
-					was an Adjunct Professor of Taxation at Pace
-					University from 1985-1989. Barry received an
-					Honorary Doctorate from Pace in 2015.
-					In 2011, Barry became a partner/owner of the New
-					York Yankees and the YES Network.
-					In 2016, Barry became a partner/owner of the Las
-					Vegas Golden Knights (NHL).
-			</p>
+
+		<div class="row">
+			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+				<!-- <img class="" src="<?php echo get_template_directory_uri(); ?>/library/images/nothumb.gif" alt="..."> -->
+				<h4 class="name">Barry Klarbrg</h4>
+				<h4>Founder</h4>
+				<p>
+						Barry Klarberg, a CPA, formed Monarch Wealth and Business Management LLC in 2008,
+						a full service Family Office firm specializing in providing Business and Wealth
+						Management Services to High Net-Worth
+						Individuals, Athletes and Entertainers.
+						Preceding Monarch, Barry spent two years as Senior
+						Managing Director at Guggenheim Partners, LLC
+						heading up Guggenheim Wealth Services New York
+						and Los Angeles offices. His team managed over 100
+						client relationships. Prior to joining Guggenheim
+						Partners, Barry was responsible for managing Assante
+						Wealth Services and Loring Ward Private Family Office
+						from January 1, 2000 to August 15, 2006.
+						Barry began his career at Deloitte in 1981, where he
+						spent 11 years. He was the Firm's National Director of
+						Sports and Entertainment. In 1992, he formed KRT
+						Business Management, specializing in Wealth
+						Management Services which he eventually sold to
+						Assante.
+					</br></br>
+						Barry has been internationally recognized as a leader in
+						the area of financial counseling to Athletes,
+						Entertainers, High Net Worth individuals and Family
+						Offices. Barry was elected to the "Sports Business
+						Journals' 40 under 40." He regularly appears on CNN,
+						CNBC and The Wall Street Journal Report. Barry is
+						actively involved in various charity and community
+						activities including the National Meningitis
+						Foundation.
+						Barry received a BBA (1981) and MS in Taxation (1985)
+						from Pace University and maintains his Series 65. He
+						was an Adjunct Professor of Taxation at Pace
+						University from 1985-1989. Barry received an
+						Honorary Doctorate from Pace in 2015.
+						In 2011, Barry became a partner/owner of the New
+						York Yankees and the YES Network.
+						In 2016, Barry became a partner/owner of the Las
+						Vegas Golden Knights (NHL).
+				</p>
+			</div>
+			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+				<!-- <img class="" src="<?php echo get_template_directory_uri(); ?>/library/images/nothumb.gif" alt="..."> -->
+				<h4 class="name">Josh Klein</h4>
+				<h4>Partner and Managing Director</h4>
+				<p>
+						Joshua Klein is a Partner and Managing Director of Monarch
+						Wealth and Business Management LLC. Monarch is a full
+						service family office firm specializing in providing Business
+						and Wealth Management Services to High Net worth
+						Individuals, Athletes, and Entertainers.
+						Joshua started his career in Business Management working as
+						an intern for Assante Wealth Management while he was
+						attending Indiana University where he later graduated with a
+						bachelor’s degree in finance from the Kelly School of Business.
+						Immediately after graduating, he moved to New York to begin
+						work for Assante’s new successor Loring Ward.
+						At Loring Ward, Joshua learned the business from the ground
+						up, beginning as an account executive handling some of music
+						and sport’s top performers. Interacting with all aspects of the
+						business, Joshua spent the majority of his time working on
+						accounting for music touring, record labels, publishing, and
+						multi state taxation for athletes.
+					</br> </br>
+						After working at Loring Ward for a 2 years, Joshua joined
+						Guggenheim Partners, LLC where he took on a new role as an
+						Account Manager handling client’s directly and managing a
+						staff of 8. In 2009, Joshua decided to leave Guggenheim and
+						form Monarch with two other partners.
+						Since joining Monarch, Joshua has become a nationally
+						recognized leader in the area of business management with an
+						emphasis on business management for CEO’s, athletes,
+						musicians, artist manager’s, and music producers. In his spare
+						time, he is actively involved the community organizations such
+						as National Meningitis Foundation as well as the Alzheimer’s
+						Association. He currently sits on the advisory board of the
+						Clive Davis Institute of Recorded Music at NYU’s Tisch School
+						of the Arts.
+				</p>
+			</div>
+
 		</div>
-		<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-			<!-- <img class="" src="<?php echo get_template_directory_uri(); ?>/library/images/nothumb.gif" alt="..."> -->
-			<h4 class="name">Josh Klein</h4>
-			<h4>Partner and Managing Director</h4>
-			<p>
-					Joshua Klein is a Partner and Managing Director of Monarch
-					Wealth and Business Management LLC. Monarch is a full
-					service family office firm specializing in providing Business
-					and Wealth Management Services to High Net worth
-					Individuals, Athletes, and Entertainers.
-					Joshua started his career in Business Management working as
-					an intern for Assante Wealth Management while he was
-					attending Indiana University where he later graduated with a
-					bachelor’s degree in finance from the Kelly School of Business.
-					Immediately after graduating, he moved to New York to begin
-					work for Assante’s new successor Loring Ward.
-					At Loring Ward, Joshua learned the business from the ground
-					up, beginning as an account executive handling some of music
-					and sport’s top performers. Interacting with all aspects of the
-					business, Joshua spent the majority of his time working on
-					accounting for music touring, record labels, publishing, and
-					multi state taxation for athletes.
-				</br> </br>
-					After working at Loring Ward for a 2 years, Joshua joined
-					Guggenheim Partners, LLC where he took on a new role as an
-					Account Manager handling client’s directly and managing a
-					staff of 8. In 2009, Joshua decided to leave Guggenheim and
-					form Monarch with two other partners.
-					Since joining Monarch, Joshua has become a nationally
-					recognized leader in the area of business management with an
-					emphasis on business management for CEO’s, athletes,
-					musicians, artist manager’s, and music producers. In his spare
-					time, he is actively involved the community organizations such
-					as National Meningitis Foundation as well as the Alzheimer’s
-					Association. He currently sits on the advisory board of the
-					Clive Davis Institute of Recorded Music at NYU’s Tisch School
-					of the Arts.
-			</p>
+		<div class="row">
+			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+				<!-- <img class="" src="<?php echo get_template_directory_uri(); ?>/library/images/nothumb.gif" alt="..."> -->
+				<h4 class="name">Tom Fitzgerald</h4>
+				<h4>Partner</h4>
+				<p>
+					Lorem ipsum dolor sit amet, an molestiae deseruisse vis. An eum error apeirian, eu odio perfecto duo.
+					Eam integre periculis evertitur an, movet populo scribentur nam ut. Qui ferri fuisset no, adhuc facilisis
+					no vis, molestie scribentur ea sea. Cibo impetus eos te, has singulis adolescens dissentiunt at. Enim
+					viderer deleniti eu nec, vis putant dicunt in. Et melius disputando eam. Ipsum mundi reformidans et vel.
+					At noluisse perpetua volutpat eam, quo dicta patrioque id, facer nusquam vim in.
+				</p>
+			</div>
+			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+				<!-- <img class="" src="<?php echo get_template_directory_uri(); ?>/library/images/nothumb.gif" alt="..."> -->
+				<h4 class="name">Harry Tawil</h4>
+				<h4>Principal</h4>
+				<p>
+					Lorem ipsum dolor sit amet, an molestiae deseruisse vis. An eum error apeirian, eu odio perfecto duo.
+					Eam integre periculis evertitur an, movet populo scribentur nam ut. Qui ferri fuisset no, adhuc facilisis
+					no vis, molestie scribentur ea sea. Cibo impetus eos te, has singulis adolescens dissentiunt at. Enim
+					viderer deleniti eu nec, vis putant dicunt in. Et melius disputando eam. Ipsum mundi reformidans et vel.
+					At noluisse perpetua volutpat eam, quo dicta patrioque id, facer nusquam vim in.
+				</p>
+			</div>
 		</div>
 
-		<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-			<!-- <img class="" src="<?php echo get_template_directory_uri(); ?>/library/images/nothumb.gif" alt="..."> -->
-			<h4 class="name">Tom Fitzgerald</h4>
-			<h4>Partner</h4>
-			<p>
-				Lorem ipsum dolor sit amet, an molestiae deseruisse vis. An eum error apeirian, eu odio perfecto duo.
-				Eam integre periculis evertitur an, movet populo scribentur nam ut. Qui ferri fuisset no, adhuc facilisis
-				no vis, molestie scribentur ea sea. Cibo impetus eos te, has singulis adolescens dissentiunt at. Enim
-				viderer deleniti eu nec, vis putant dicunt in. Et melius disputando eam. Ipsum mundi reformidans et vel.
-				At noluisse perpetua volutpat eam, quo dicta patrioque id, facer nusquam vim in.
-			</p>
-		</div>
-		<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-			<!-- <img class="" src="<?php echo get_template_directory_uri(); ?>/library/images/nothumb.gif" alt="..."> -->
-			<h4 class="name">Harry Tawil</h4>
-			<h4>Principal</h4>
-			<p>
-				Lorem ipsum dolor sit amet, an molestiae deseruisse vis. An eum error apeirian, eu odio perfecto duo.
-				Eam integre periculis evertitur an, movet populo scribentur nam ut. Qui ferri fuisset no, adhuc facilisis
-				no vis, molestie scribentur ea sea. Cibo impetus eos te, has singulis adolescens dissentiunt at. Enim
-				viderer deleniti eu nec, vis putant dicunt in. Et melius disputando eam. Ipsum mundi reformidans et vel.
-				At noluisse perpetua volutpat eam, quo dicta patrioque id, facer nusquam vim in.
-			</p>
-		</div>
 
-	</div>
 </div>
 
 <div class="row about-lower ">
