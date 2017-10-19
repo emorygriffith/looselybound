2fdecea36bf3b00bb29ac721213c17499303bed9
Emory Griffith <emorygriffith91@gmail.com>
2017-10-19 16:11:28 -0400

    re-doing the team page to be center aligned, with photos, and on white background. still need to add the 2 additional people

--- page-team.php
+++ page-team.php
@@ -8,128 +8,213 @@
 
 <div class="row col-lg-9 page-header contain">
 	<h1>Our Team</h1>
-	<h2>Lorum Ipsum Dolor, our team is lorum ipsum.</h2>
+	<h2>We Are Monarch</h2>
 </div>
 
 
-<div class="row col-lg-12 about-bios">
+<div class="container-fluid about">
 
-		<h2 id="team">TEAM</h2>
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<img class="bio-pic" src="<?php echo get_template_directory_uri(); ?>/library/images/emory_griffith.jpg" alt="...">
+					
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<h4 class="name">Barry Klarberg</h4>
+					<p>Founder and Managing Partner</p>
 
-		<div class="row">
-			<div class="bios col-xl-9 col-lg-9 col-md-9 col-sm-9">
-				<h4 class="name">Barry Klarberg</h4>
-				<h4>Founder and Managing Partner</h4>
-				<p>
-					In 2008, Barry Klarberg formed Monarch Wealth and Business Management LLC, a full service Family Office firm.
-					</br> </br>
-					Barry received a BBA and MS in Taxation as well as an Honorary Doctorate from Pace University and maintains his Series 65.
-					He was an Adjunct Professor of Taxation at Pace University from 1985-1989. In the Fall of 2017, Klarberg
-					will teach "Business Developments in Sports" at the University of Michigan in Ann Arbor.
-					</br> </br>
-					Barry began his career at Deloitte in 1981, where he spent 11 years as the National Director of Sports and Entertainment.
-					In 1992, he formed KRT Business Management, a firm specializing in Wealth Management Services
-					which he eventually sold to Assante.
-					</br> </br>
-					Preceding Monarch, Barry spent two years as Senior Managing Director at Guggenheim Partners, LLC,
-					heading up Guggenheim Wealth Services in New York and Los Angeles offices.
-					His team managed over 100 client relationships during his tenure. Prior to joining Guggenheim Partners,
-					Barry was responsible for managing Assante Wealth Services and Loring Ward Private Family Office.
-					</br> </br>
-					Barry has been internationally recognized as a leader in the area of financial counseling to Athletes,
-					Entertainers, and High Net Worth individuals. Barry was elected to the
-					"Sports Business Journals' 40 under 40." He regularly appears on CNN, CNBC and The Wall Street Journal Report.
-					</br> </br>
-					Barry is also a partner/owner of the New York Yankees, YES Network, New York FC (MLS)
-					and Las Vegas Golden Knights (NHL).
-				</p>
+					<p>
+						In 2008, Barry Klarberg formed Monarch Wealth and Business Management LLC, a full service Family Office firm.
+						</br> </br>
+						Barry received a BBA and MS in Taxation as well as an Honorary Doctorate from Pace University and maintains his Series 65.
+						He was an Adjunct Professor of Taxation at Pace University from 1985-1989. In the Fall of 2017, Klarberg
+						will teach "Business Developments in Sports" at the University of Michigan in Ann Arbor.
+						</br> </br>
+						Barry began his career at Deloitte in 1981, where he spent 11 years as the National Director of Sports and Entertainment.
+						In 1992, he formed KRT Business Management, a firm specializing in Wealth Management Services
+						which he eventually sold to Assante.
+						</br> </br>
+						Preceding Monarch, Barry spent two years as Senior Managing Director at Guggenheim Partners, LLC,
+						heading up Guggenheim Wealth Services in New York and Los Angeles offices.
+						His team managed over 100 client relationships during his tenure. Prior to joining Guggenheim Partners,
+						Barry was responsible for managing Assante Wealth Services and Loring Ward Private Family Office.
+						</br> </br>
+						Barry has been internationally recognized as a leader in the area of financial counseling to Athletes,
+						Entertainers, and High Net Worth individuals. Barry was elected to the
+						"Sports Business Journals' 40 under 40." He regularly appears on CNN, CNBC and The Wall Street Journal Report.
+						</br> </br>
+						Barry is also a partner/owner of the New York Yankees, YES Network, New York FC (MLS)
+						and Las Vegas Golden Knights (NHL).
+					</p>
+				</div>
 			</div>
 
-			<div class="bios col-xl-9 col-lg-9 col-md-9 col-sm-9">
-				<h4 class="name">Josh Klein</h4>
-				<h4>Partner and Managing Director</h4>
-				<p>
-					Joshua Klein is a Partner and Managing Director of Monarch Business and Wealth Management LLC.
-					</br> </br>
-					Joshua started his career in Business Management working as an intern for Assante
-					Wealth Management while attending the Kelly School of Business at Indiana University.
-					Immediately after graduating, he moved to New York to begin work for Assante’s successor Loring Ward.
-					</br></br>
-					At Loring Ward, Joshua learned the business from the ground up, beginning as an account
-					executive handling the financial lives of some of music and sport’s top performers.
-					Interacting with all aspects of the business, Joshua spent the majority of his time
-					working on accounting for music tours, record labels, publishing, and multi-state
-					taxation for athletes.
-					</br></br>
-					Joshua then joined Guggenheim Partners, LLC, where he took on a new role as an Account Manager,
-					handling clients directly and managing a staff of 8. In 2009, Joshua decided to leave
-					Guggenheim and form Monarch with two other partners.
-					</br></br>
-					Since forming Monarch, Joshua has become a nationally recognized leader in the area of
-					business management. He has been actively involved in a number of philanthropic and
-					educational initiatives such as the National Meningitis Foundation, the Alzheimer’s
-					Association and most recently he sat on the advisory board of the Clive Davis
-					Institute of Recorded Music at NYU’s Tisch School of the Arts.
-				</p>
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<img class="bio-pic" src="<?php echo get_template_directory_uri(); ?>/library/images/emory_griffith.jpg" alt="...">
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<h4 class="name">Josh Klein</h4>
+					<p>Partner and Managing Director</p>
+
+					<p>
+						Joshua Klein is a Partner and Managing Director of Monarch Business and Wealth Management LLC.
+						</br> </br>
+						Joshua started his career in Business Management working as an intern for Assante
+						Wealth Management while attending the Kelly School of Business at Indiana University.
+						Immediately after graduating, he moved to New York to begin work for Assante’s successor Loring Ward.
+						</br></br>
+						At Loring Ward, Joshua learned the business from the ground up, beginning as an account
+						executive handling the financial lives of some of music and sport’s top performers.
+						Interacting with all aspects of the business, Joshua spent the majority of his time
+						working on accounting for music tours, record labels, publishing, and multi-state
+						taxation for athletes.
+						</br></br>
+						Joshua then joined Guggenheim Partners, LLC, where he took on a new role as an Account Manager,
+						handling clients directly and managing a staff of 8. In 2009, Joshua decided to leave
+						Guggenheim and form Monarch with two other partners.
+						</br></br>
+						Since forming Monarch, Joshua has become a nationally recognized leader in the area of
+						business management. He has been actively involved in a number of philanthropic and
+						educational initiatives such as the National Meningitis Foundation, the Alzheimer’s
+						Association and most recently he sat on the advisory board of the Clive Davis
+						Institute of Recorded Music at NYU’s Tisch School of the Arts.
+					</p>
+				</div>
 			</div>
 
-		</div>
-		<div class="row">
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<img class="bio-pic" src="<?php echo get_template_directory_uri(); ?>/library/images/emory_griffith.jpg" alt="...">
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<h4 class="name">Tom Fitzgerald</h4>
+					<p>Partner</p>
 
-			<div class="bios col-xl-9 col-lg-9 col-md-9 col-sm-9">
-				<h4 class="name">Tom Fitzgerald</h4>
-				<h4>Partner</h4>
-				<p>
-					Tom Fitzgerald is a partner and managing director of Monarch Business and Wealth Management LLC.
-					He has been with Monarch since inception. He spent the first 12 years of his career in
-					public accounting (including two years at PriceWaterhouseCoopers, two years at Deloitte
-					and eight years at Ernst & Young LLP).
-					</br></br>
-					At Monarch, Tom specializes in the area of business management for athletes,
-					athlete executives, musicians, models and music producers.
-					</br></br>
-					Tom graduated in 1988 with a bachelor’s degree in Business Administration
-					with a concentration in Accounting from Saint Bonaventure University (Olean, NY).
-					He has served on the United States Olympic Committee’s Board of Directors and
-					was an active member of their Accounting and Finance Committee for four years.
-					</br></br>
-					Tom is actively involved in several church and charitable organizations in Manhattan.
-					He enjoys surfing, yoga and travelling. He has lived in Moscow, Prague and
-					Stockholm and has traveled to over 30 countries in the world.
-					</br></br>
-				</p>
+					<p>
+						Tom Fitzgerald is a partner and managing director of Monarch Business and Wealth Management LLC.
+						He has been with Monarch since inception. He spent the first 12 years of his career in
+						public accounting (including two years at PriceWaterhouseCoopers, two years at Deloitte
+						and eight years at Ernst & Young LLP).
+						</br></br>
+						At Monarch, Tom specializes in the area of business management for athletes,
+						athlete executives, musicians, models and music producers.
+						</br></br>
+						Tom graduated in 1988 with a bachelor’s degree in Business Administration
+						with a concentration in Accounting from Saint Bonaventure University (Olean, NY).
+						He has served on the United States Olympic Committee’s Board of Directors and
+						was an active member of their Accounting and Finance Committee for four years.
+						</br></br>
+						Tom is actively involved in several church and charitable organizations in Manhattan.
+						He enjoys surfing, yoga and travelling. He has lived in Moscow, Prague and
+						Stockholm and has traveled to over 30 countries in the world.
+						</br></br>
+					</p>
+				</div>
 			</div>
 
-			<div class="bios col-xl-9 col-lg-9 col-md-9 col-sm-9">
-				<h4 class="name">Harry Tawil</h4>
-				<h4>Principal</h4>
-				<p>
-					Harry Tawil is a Principal at Monarch Business & Wealth Management as well as a Partner at
-					GMM Capital LLC. Harry graduated from Baruch College in New York City with a B.B.A. in
-					Economics and a minor in Political Science.
-					</br> </br>
-					Before graduating, Harry started Kinersis Renewables, an independent power producer focused
-					on the development, and operation of utility scale wind and solar projects spanning six
-					countries and three continents. After a successful exit, Harry joined GMM Capital LLC as a partner.
-					Harry manages the investment team, funding numerous entities both public and private.
-					These include Delta Galil (TASE: DELT), a global manufacturer of apparel for Nike, Lululemon,
-					as well as for Delta owned 7 For All Mankind & Splendid. Delta has annual sales exceeding $1.5 billion.
-					</br> </br>
-					At Monarch, Harry focuses on the macro level, developing business strategies,
-					referring top level clients, and reviewing private equity transactions for clients.
-					Since joining Monarch in 2016, Harry has contributed to the ever-growing client list
-					and steady success of Monarch.
-					</br> </br>
-					Harry lives in New York City with his wife and daughter. He is actively involved with
-					various non-profit organizations including the Special Children’s Center and the Safra
-					Synagogue of Manhattan. In Harry’s spare time he enjoys playing tennis and is an
-					avid music and sports fan.
-				</p>
-			</div>
-		</div>
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<img class="bio-pic" src="<?php echo get_template_directory_uri(); ?>/library/images/emory_griffith.jpg" alt="...">
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<h4 class="name">Harry Tawil</h4>
+					<p>Principal</p>
 
+					<p>
+						Harry Tawil is a Principal at Monarch Business & Wealth Management as well as a Partner at
+						GMM Capital LLC. Harry graduated from Baruch College in New York City with a B.B.A. in
+						Economics and a minor in Political Science.
+						</br> </br>
+						Before graduating, Harry started Kinersis Renewables, an independent power producer focused
+						on the development, and operation of utility scale wind and solar projects spanning six
+						countries and three continents. After a successful exit, Harry joined GMM Capital LLC as a partner.
+						Harry manages the investment team, funding numerous entities both public and private.
+						These include Delta Galil (TASE: DELT), a global manufacturer of apparel for Nike, Lululemon,
+						as well as for Delta owned 7 For All Mankind & Splendid. Delta has annual sales exceeding $1.5 billion.
+						</br> </br>
+						At Monarch, Harry focuses on the macro level, developing business strategies,
+						referring top level clients, and reviewing private equity transactions for clients.
+						Since joining Monarch in 2016, Harry has contributed to the ever-growing client list
+						and steady success of Monarch.
+						</br> </br>
+						Harry lives in New York City with his wife and daughter. He is actively involved with
+						various non-profit organizations including the Special Children’s Center and the Safra
+						Synagogue of Manhattan. In Harry’s spare time he enjoys playing tennis and is an
+						avid music and sports fan.
+					</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<img class="bio-pic" src="<?php echo get_template_directory_uri(); ?>/library/images/emory_griffith.jpg" alt="...">
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<h4 class="name">Jessica Harrison</h4>
+					<p>Vice President</p>
+
+					<p>
+						Jessica Harrison is Vice President of Monarch Business & Wealth Management LLC.
+						And has been with Monarch since 2011.
+						</br><br>
+						Jessica started her career in Business Management as an intern at Monarch while attending Providence College.
+						After graduating from Providence with her bachelors in marketing,
+						she continued her education at Hofstra University.  Jessica earned her
+						MBA in Sports and Entertainment Management and began working at Monarch
+						immediately following graduation.
+					</br><br>
+						At Monarch, Jessica was introduced to Business Management from the ground up.
+						She has worked with professional athletes and sports executives as well as some
+						of the biggest names in music, specializing in entertainment and touring.
+						She currently works with solo artists and performing acts, handling all
+						aspects of their careers including publishing, label relations, merchandising and taxation.
+					</br><br>
+						In her spare time, Jessica enjoys yoga, running, attending concerts and traveling.
+						She has visited exotic areas in Asia and Africa. She supports The American Cancer
+						Society through various endeavors including their Young Professional Volunteer Program.
+					</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<img class="bio-pic" src="<?php echo get_template_directory_uri(); ?>/library/images/emory_griffith.jpg" alt="...">
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<h4 class="name">Matt Lastname</h4>
+					<p>Position</p>
+
+					<p>
+						Jessica Harrison is Vice President of Monarch Business & Wealth Management LLC.
+						And has been with Monarch since 2011.
+						</br><br>
+						Jessica started her career in Business Management as an intern at Monarch while attending Providence College.
+						After graduating from Providence with her bachelors in marketing,
+						she continued her education at Hofstra University.  Jessica earned her
+						MBA in Sports and Entertainment Management and began working at Monarch
+						immediately following graduation.
+					</br><br>
+						At Monarch, Jessica was introduced to Business Management from the ground up.
+						She has worked with professional athletes and sports executives as well as some
+						of the biggest names in music, specializing in entertainment and touring.
+						She currently works with solo artists and performing acts, handling all
+						aspects of their careers including publishing, label relations, merchandising and taxation.
+					</br><br>
+						In her spare time, Jessica enjoys yoga, running, attending concerts and traveling.
+						She has visited exotic areas in Asia and Africa. She supports The American Cancer
+						Society through various endeavors including their Young Professional Volunteer Program.
+					</p>
+				</div>
+			</div>
 
 </div>
+
+
+
+
+
+
 
 <div class="row about-lower ">
 	<a href="/services">
